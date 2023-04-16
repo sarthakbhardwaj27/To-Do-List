489895c0a362ad880dbe9ea2ce09f9b6127dda95
SarthakBhardwajCode <85218961+sarthakbhardwaj27@users.noreply.github.com>
2023-04-17 00:07:24 +0530

    trying to add css

--- main.js
+++ main.js
@@ -9,8 +9,9 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use(express.static("public", { "Content-Type": "text/css" }));
+// app.use(express.static("public"));
+// app.use(express.static("public", { "Content-Type": "text/css" }));
+app.use(express.static(__dirname + '/public/'));
 
 mongoose.connect("mongodb://localhost:27017/todolistDB", {
   useNewUrlParser: true,
