3bd86a5ed9be2c45d6558b2820e5a600104f821a
SarthakBhardwajCode <85218961+sarthakbhardwaj27@users.noreply.github.com>
2023-04-24 15:15:58 +0530

    redid the app

--- main.js
+++ main.js
@@ -1,162 +1,184 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-let ejs = require("ejs");
-
 const app = express();
-// var items = [];
-// let workItems = [];
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static("public"));
-// app.use(express.static("public", { "Content-Type": "text/css" }));
-app.use(express.static(__dirname + '/public/'));
+const port = 3200;
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
-  useNewUrlParser: true,
-});
-const itemsSchema = {
-  name: String,
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+//misc
+// var tasks=[];
+// const item1="Create Project";
+// const item2="Do DSA";
+// const item3="Workout";
+// tasks.concat(item1);
+// tasks.concat(item2);
+// tasks.concat(item3);
+
+
+//mongodb
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+//creating mongoose schema
+const taskSchema ={
+    task: String
 };
 
-const Item = mongoose.model("Item", itemsSchema);
+//mongoose model or collection
+const Task = mongoose.model(
+    "Task",
+    taskSchema
+);
 
-const item1 = new Item({
-  name: "Welcome to your todoList",
-});
-
-const item2 = new Item({
-  name: "Hit the plus button to add a new item",
-});
-
-const item3 = new Item({
-  name: " Hit this button to delete",
-});
-
-const defaultItems = [item1, item2, item3];
-
-// Item.insertMany(defaultItems, function(err){
-//   if(err){
-//     console.log(err);
-//   }else{
-//     console.log("Successfully saved default items to DB.");
-//   }
+//mongoose document
+// const task1 = new Task({
+//     task: "Go to Gym"
 // })
-// insertmany no longer accepts a callback
+// const task2 = new Task({
+//     task: "Work on project"
+// })
+// const task3 = new Task({
+//     task: "be grateful"
+// })
 
-try {
-  const result = Item.insertMany(defaultItems);
-  console.log(`${result.insertedCount} documents inserted`);
-} catch (error) {
-  console.error(error);
+
+// const defaultTasks = [task1,task2,task3];
+
+// Task.insertMany(defaultTasks)
+// since we are getting multiple copies of the data in our db at each server run, we will put an if statement in app.get, task.find method. that is if founditems list is empty then only add default items to the list
+// try{
+//     Task.insertMany(defaultTasks);
+//     console.log("Successfully added default items to DB");
+// }catch(err){
+//     console.log(err);
+// }
+
+
+function findday() {
+    let date = new Date();
+    let today = date.getDay();
+    let day;
+
+    switch (today) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+    }
+    return day;
 }
 
-
-// app.get("/",function(req,res){
-//     var today = new Date();
-//     var currentday = today.getDay();
-
-//     if(currentday === 6 || currentday === 0)
-//         res.send("it is a weekend");
-//     else{
-//         // res.send("arghh go to work");
-//         res.sendFile(__dirname+"/index.html");
-//     }
-
-//     // if(currentday===6 || currentday===0){
-//     //     res.send("<h1>yayyy chutti haiii</h1>")
-//     // }
-//     // else{
-//     //     res.write("Today is working day");
-//     //     res.write("booo i have to work");
-//     //     res.send();
-//     // }
-// });
-
 app.get("/", (req, res) => {
-  var today = new Date();
-  var currentday = today.getDay();
-  var day = "";
+    //res.send("<h1>Home</h1>");
+    let day = findday();
+    //res.send(`<h1>${day}'s To Do List</h1>`);
 
-  //   switch (currentday) {
-  //     case 0:
-  //       day = "Sunday";
-  //       break;
-  //     case 1:
-  //       day = "Monday";
-  //       break;
-  //     case 2:
-  //       day = "Tuesday";
-  //       break;
-  //     case 3:
-  //       day = "Wednesday";
-  //       break;
-  //     case 4:
-  //       day = "Thursday";
-  //       break;
-  //     case 5:
-  //       day = "Friday";
-  //       break;
-  //     case 6:
-  //       day = "Saturday";
-  //   }
-  //https://stackoverflow.com/questions/3552461/how-do-i-format-a-date-in-javascript
-  var options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-  var today = new Date();
+    //mongoose find
+    // const founditems = Task.find({}).orFail(new Error('No docs found!'));
+    //var founditems = Task.find({});
 
-  day = today.toLocaleDateString("en-US", options); // Saturday, September 17, 2016
-
-  // Item.find({}).exec();
-
-  
-
-  Item.find({}).then(function (foundItems) {
-    if(foundItems.length === 0){
-      Item.insertMany(defaultItem);
-      res.render("list", { _listTitle: "Today", newListItems: foundItems });
-    }else{
-      res.render("list", { _listTitle: "Today", newListItems: foundItems });
-    }
-    }).catch(function (err) {
+    Task.find({})
+    .then(founditems => {
+        if(founditems.length===0){
+            try{
+                Task.insertMany(defaultTasks);
+                console.log("Successfully added default items to DB");
+            }
+            catch(err){
+                console.log(err);
+            }
+            res.redirect("/");
+        }
+        else{
+            res.render('list', { _day: day, _tasks: founditems });
+        }
+      
+    })
+    .catch(err => {
       console.log(err);
-  }); 
+      res.status(500).send("Error retrieving tasks from database.");
+    });
+    // Task.find({},function(err,founditems){
+    //     if(err)
+    //         console.log(err);
+    //     else
+    //         console.log(founditems);
+    // })
+
+
+    // res.sendFile(__dirname+"/index.html") using this we can send index.html files
+    // res.render('list',{_day:day, _tasks:tasks});
+
+    // res.render('list',{_day:day, _tasks:founditems})
 });
-  //res.render("list", { _listTitle: day , newListItems: items});
-// });
 
-app.get("/work", (req, res) => {
-  res.render("list", { _listTitle: "Work List", newListItems: workItems });
-});
+app.post("/",(req,res)=>{
+    var taskName = req.body.newItem;
+    console.log(taskName);
 
-// app.post("/work",(req,res)=>{
-//   let item = req.body.newItem;
-//   workItems.push(item);
-//   res.redirect("/work");
-// })
+    const newTask = new Task({
+        task: taskName
+    });
 
-app.post("/", (req, res) => {
-  var item = req.body.newItem;
-  // console.log(item);
-  console.log(req.body);
-  if (req.body.button === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
+    newTask.save();
+
+    // tasks.push(task);
+    // console.log(task);
+    //res.send("Form submitted successfully")
+
+    //trying to send data to mongo directly
+    // try{
+    //     Task.insertOne(task);
+    //     console.log("Successfully aaded task to DB");
+    // }catch(err){
+    //     console.log(err);
+    // }
+
     res.redirect("/");
-  }
-});
+})
+app.post("/delete",async (req,res)=>{
+    // console.log(req.body.checkbox);
+    const checkedTaskId = req.body.checkbox;
+    // Task.deleteOne(checkedTaskId);
+    // console.log(`${checkedTaskId} deleted`);
+    // Task.findByIdAndDelete(checkedTaskId,(err)=>{
+    //     if(err)
+    //         console.log(err);
+    //     else
+    //         console.log(`${checkedTaskId} deleted`);
+    // })
+    try {
+        await Task.findByIdAndDelete(checkedTaskId);
+        console.log(`${checkedTaskId} deleted`);
+      } catch (err) {
+        console.error(err);
+      }
+      res.redirect("/");
+})
 
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-
-app.listen(3000, function () {
-  console.log("Server is now started on port 3000");
+app.listen(port, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(`Server is listening on port ${port}`);
+    }
 });
